915a4e8c02d0417c6b37b1aaa4b7c5970c081f08
Reshmas23 <reshmasuresh8089@gmail.com>
2024-04-13 15:10:11 +0530

    april 13 first commit --@reshma

--- lib/view/users/admin/screens/achievements/achievements.dart
+++ lib/view/users/admin/screens/achievements/achievements.dart
@@ -1,5 +1,6 @@
 import 'package:awesome_side_sheet/Enums/sheet_position.dart';
 import 'package:awesome_side_sheet/side_sheet.dart';
+import 'package:file_picker/file_picker.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:google_fonts/google_fonts.dart';
@@ -16,10 +17,16 @@ import 'package:vidyaveechi_website/view/utils/shared_pref/user_auth/user_creden
 import 'package:vidyaveechi_website/view/widgets/responsive/responsive.dart';
 import 'package:vidyaveechi_website/view/widgets/routeSelectedTextContainer/routeSelectedTextContainer.dart';
 
-class Achievements extends StatelessWidget {
+class Achievements extends StatefulWidget {
+  Achievements({super.key});
+
+  @override
+  State<Achievements> createState() => _AchievementsState();
+}
+
+class _AchievementsState extends State<Achievements> {
   final AchievementsController achievementsController =
       Get.put(AchievementsController());
-  Achievements({super.key});
 
   @override
   Widget build(BuildContext context) {
@@ -79,70 +86,71 @@ class Achievements extends StatelessWidget {
     return Container(
       height: 1000,
       color: screenContainerbackgroundColor,
-      child: Form(
-        key: achievementsController.formKey,
-        child: Padding(
-          padding: const EdgeInsets.only(left: 10),
-          child: SingleChildScrollView(
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                const Padding(
-                  padding: EdgeInsets.only(left: 25, top: 25),
-                  child: Row(
-                    children: [
-                      TextFontWidget(
-                        text: 'Achievements',
-                        fontsize: 18,
-                        fontWeight: FontWeight.bold,
-                      ),
-                    ],
-                  ),
+      child: Padding(
+        padding: const EdgeInsets.only(left: 10),
+        child: SingleChildScrollView(
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              const Padding(
+                padding: EdgeInsets.only(left: 25, top: 25),
+                child: Row(
+                  children: [
+                    TextFontWidget(
+                      text: 'Achievements',
+                      fontsize: 18,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ],
                 ),
-                Padding(
-                  padding: const EdgeInsets.only(top: 10),
-                  child: Row(
-                    children: [
-                      GestureDetector(
-                          onTap: () {
-                            {
-                              aweSideSheet(
-                                  context: context,
-                                  sheetPosition: SheetPosition.right,
-                                  title: "Acheivements",
-                                  body: Padding(
-                                    padding: const EdgeInsets.only(
-                                        left: 10, top: 10),
-                                    child: SingleChildScrollView(
+              ),
+              Padding(
+                padding: const EdgeInsets.only(top: 10),
+                child: Row(
+                  children: [
+                    GestureDetector(///////////////////////////////////////////////////////////////////////////////////
+                        onTap: () {
+                          
+                            aweSideSheet(
+                                context: context,
+                                sheetPosition: SheetPosition.right,
+                                title: "Acheivements",
+                                body: Padding(
+                                  padding: const EdgeInsets.only(  left: 10, top: 10),
+                                  child: SingleChildScrollView(
+                                    child: Form(
+                                      key: achievementsController.formKey,
                                       child: Column(
                                         children: [
                                           SizedBox(
                                               height: 800,
                                               child: Container(
-                                                margin: const EdgeInsets.only(
-                                                    top: 10),
+                                                margin: const EdgeInsets.only( top: 10),
                                                 child:
                                                     //  Obx(() {
                                                     // return
                                                     Column(
-                                                        mainAxisAlignment:
-                                                            MainAxisAlignment
-                                                                .center,
-                                                        crossAxisAlignment:
-                                                            CrossAxisAlignment
-                                                                .center,
+                                                        mainAxisAlignment:  MainAxisAlignment.center,
+                                                        crossAxisAlignment: CrossAxisAlignment .center,
                                                         children: [
                                                       Stack(
                                                         children: [
                                                           // Obx(() {
-                                                          const CircleAvatar(
-                                                            radius: 60,
-                                                            // backgroundImage: Image.network(
-                                                            //         signupController.userImage.value)
-                                                            //
-                                                            //   .image,
-                                                          ),
-
+                                                          (achievementsController.afile == null)
+                                                              ? const CircleAvatar(
+                                                                  radius: 60,
+                                                                  backgroundImage:
+                                                                      NetworkImage( 'https://via.placeholder.com/150'),
+                                                                  backgroundColor:  Color.fromARGB( 241, 54, 225, 248),
+                                                                )
+                                                              : CircleAvatar(
+                                                                  radius: 60,
+                                                                  backgroundImage:
+                                                                      MemoryImage( achievementsController .afile!)
+                                                                  //
+                                                                  //   .image,
+                                                                  ),
+                                            
                                                           // }
                                                           // }),
                                                           Positioned(
@@ -150,28 +158,39 @@ class Achievements extends StatelessWidget {
                                                               left: 80,
                                                               child: IconButton(
                                                                   onPressed:
-                                                                      () {
-                                                                    // signupController.uploadImg();
-                                                                    // // signupController.uploadImage;
+                                                                      () async {
+                                                                    FilePickerResult?
+                                                                        result =
+                                                                        await FilePicker
+                                                                            .platform
+                                                                            .pickFiles(type: FileType.image);
+                                                                    if (result !=
+                                                                        null) {
+                                                                      achievementsController.file = result
+                                                                          .files
+                                                                          .first
+                                                                          .bytes;
+                                                                      setState(
+                                                                          () {
+                                                                        achievementsController.afile =
+                                                                            achievementsController.file;
+                                                                      });
+                                                                    }
                                                                   },
                                                                   icon: const Icon(
-                                                                      Icons
-                                                                          .add_a_photo)))
+                                                                      Icons  .add_a_photo)))
                                                         ],
                                                       ),
                                                       textformWidget[0],
                                                       textformWidget[1],
                                                       textformWidget[2],
-
+                                            
                                                       textformWidget[3],
                                                       textformWidget[4],
-
+                                            
                                                       Padding(
                                                         padding:
-                                                            const EdgeInsets
-                                                                .only(
-                                                                top: 10,
-                                                                bottom: 10),
+                                                            const EdgeInsets .only(  top: 10, bottom: 10),
                                                         child:
                                                             NoticeButtonContainerWidget(
                                                           text: 'Submit',
@@ -198,101 +217,82 @@ class Achievements extends StatelessWidget {
                                       ),
                                     ),
                                   ),
-                                  showCloseButton: false,
-                                  footer: Container());
-                            }
-                          },
-                          child: const RouteSelectedTextContainer(
-                              width: 140, title: 'Create')),
-                    ],
-                  ),
+                                ),
+                                showCloseButton: false,
+                                footer: Container());
+                          
+                        },/////////////////////////////////////////////////////////////create function
+                        child: const RouteSelectedTextContainer(
+                            width: 140, title: 'Create')),
+                  ],
                 ),
-                // ResponsiveWebSite.isMobile(context)
-                //     ? Container(
-                //         color: cWhite,
-                //         child: const Padding(
-                //           padding: EdgeInsets.only(top: 10),
-                //           child: Column(
-                //             children: [],
-                //           ),
-                //         ),
-                //       )
-
-                // Obx(() {
-                // return generalInsructionController.isLoading.value
-                //  ?
-                // const Center(
-                //     child: CircularProgressIndicator(),
-                //   )
-
-                // :
-                //  :
-                SingleChildScrollView(
-                  child: SizedBox(
-                    height: 1000,
-                    width: ResponsiveWebSite.isMobile(context)
-                        ? double.infinity
-                        : 1000,
-                    child:
-                        // Padding(
-                        // padding: const EdgeInsets.symmetric(
-                        //   horizontal: 100,
-                        //   vertical: 30,
-                        // ),
-                        // child:
-                        StreamBuilder(
-                            stream: server
-                                .collection('SchoolListCollection')
-                                .doc(UserCredentialsController.schoolId)
-                                .collection(UserCredentialsController.batchId!)
-                                .doc(UserCredentialsController.batchId!)
-                                .collection('AdminAchievements')
-                                .snapshots(),
-                            builder: (context, snapshot) {
-                              if (snapshot.connectionState ==
-                                  ConnectionState.waiting) {
-                                return const Center(
-                                    child: CircularProgressIndicator());
-                              }
-                              // ignore: prefer_is_empty
-                              if (snapshot.data!.docs.length == 0) {
-                                return Center(
-                                    child: Text(
-                                  'No Achievements',
-                                  style: GoogleFonts.poppins(
-                                      fontSize: 20,
-                                      fontWeight: FontWeight.w500),
-                                ));
-                              }
-                              return GridView.builder(
-                                itemCount: snapshot.data!.docs.length,
-
-                                //snapshot.data!.docs.length,
-                                itemBuilder: (context, index) {
-                                  AchievementModel data =
-                                      AchievementModel.fromMap(
-                                          snapshot.data!.docs[index].data());
-                                  return Padding(
-                                      padding: EdgeInsets.all(20.0),
-                                      child: NewAchievementsUI(
-                                        data: data,
-                                      ));
-                                },
-                                gridDelegate:
-                                    SliverGridDelegateWithFixedCrossAxisCount(
-                                        crossAxisCount:
-                                            ResponsiveWebSite.isMobile(context)
-                                                ? 1
-                                                : 2),
-                              );
-                            }),
-
-                    // ),
-                  ),
-                )
-                //})
-              ],
-            ),
+              ),
+      
+              SingleChildScrollView(
+                child: SizedBox(
+                  height: 1000,
+                  width: ResponsiveWebSite.isMobile(context)
+                      ? double.infinity
+                      : 1000,
+                  child:
+                      // Padding(
+                      // padding: const EdgeInsets.symmetric(
+                      //   horizontal: 100,
+                      //   vertical: 30,
+                      // ),
+                      // child:
+                      StreamBuilder(
+                          stream: server
+                              .collection('SchoolListCollection')
+                              .doc(UserCredentialsController.schoolId)
+                              .collection(UserCredentialsController.batchId!)
+                              .doc(UserCredentialsController.batchId!)
+                              .collection('AdminAchievements')
+                              .snapshots(),
+                          builder: (context, snapshot) {
+                            if (snapshot.connectionState ==
+                                ConnectionState.waiting) {
+                              return const Center(
+                                  child: CircularProgressIndicator());
+                            }
+                            // ignore: prefer_is_empty
+                            if (snapshot.data!.docs.length == 0) {
+                              return Center(
+                                  child: Text(
+                                'No Achievements',
+                                style: GoogleFonts.poppins(
+                                    fontSize: 20,
+                                    fontWeight: FontWeight.w500),
+                              ));
+                            }
+                            return GridView.builder(
+                              itemCount: snapshot.data!.docs.length,
+      
+                              //snapshot.data!.docs.length,
+                              itemBuilder: (context, index) {
+                                AchievementModel data =
+                                    AchievementModel.fromMap(
+                                        snapshot.data!.docs[index].data());
+                                return Padding(
+                                    padding: EdgeInsets.all(20.0),
+                                    child: NewAchievementsUI(
+                                      data: data,
+                                    ));
+                              },
+                              gridDelegate:
+                                  SliverGridDelegateWithFixedCrossAxisCount(
+                                      crossAxisCount:
+                                          ResponsiveWebSite.isMobile(context)
+                                              ? 1
+                                              : 2),
+                            );
+                          }),
+      
+                  // ),
+                ),
+              )
+              //})
+            ],
           ),
         ),
       ),
