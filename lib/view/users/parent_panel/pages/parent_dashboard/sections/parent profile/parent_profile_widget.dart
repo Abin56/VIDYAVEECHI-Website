409ddefeb2b62003208c8ee11c3b5e312a0c8f84
rahullr <rahullr1320@gmail.com>
2024-04-13 15:19:52 +0530

    13 April second commit --@Rahul

---
+++ lib/view/users/parent_panel/pages/parent_dashboard/sections/parent profile/parent_profile_widget.dart
@@ -0,0 +1,52 @@
+// import 'package:dash_board/view/fonts/text_widget.dart';
+import 'package:flutter/material.dart';
+import 'package:vidyaveechi_website/view/colors/colors.dart';
+import 'package:vidyaveechi_website/view/fonts/text_widget.dart';
+
+// import '../../../../../../../../colors/colors.dart';
+
+class ParentProfileWidget extends StatelessWidget {
+  const ParentProfileWidget({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return   Container(
+                                color: cWhite,
+                                // width: 565,
+                                height: 160,
+                                child: Padding(
+                                  padding: const EdgeInsets.only(top: 10, left: 10),
+                                  child: Row(
+                                    children: [
+                                      const Padding(
+                                        padding: EdgeInsets.only(
+                                          left: 10,
+                                          top: 10,
+                                        ),
+                                        child: CircleAvatar(
+                                          radius: 60,
+                                        ),
+                                      ),
+                                      Padding(
+                                        padding: const EdgeInsets.only(
+                                            top: 60, left: 10),
+                                        child: Column(
+                                          children: [
+                                            TextFontWidget(
+                                              text: "Neena V",
+                                              fontsize: 18,
+                                             fontWeight: FontWeight.bold,
+                                            ),
+                                            TextFontWidget(
+                                                text: "Thursday ,Nov 23 2023",
+                                                fontsize: 10),
+                                          ],
+                                        ),
+                                      ),
+                                     
+                                    ],
+                                  ),
+                                ),
+                              );
+  }
+}
