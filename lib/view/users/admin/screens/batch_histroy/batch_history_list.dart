cee204fde9b557b506aaa6914055b60f63935261
SruthiSadanandan <sruthipariyapuram@gmail.com>
2024-04-17 18:08:45 +0530

    17/04/24 6:11

--- lib/view/users/admin/screens/batch_histroy/batch_history_list.dart
+++ lib/view/users/admin/screens/batch_histroy/batch_history_list.dart
@@ -1,10 +1,6 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
-import 'package:get/get.dart';
-import 'package:vidyaveechi_website/controller/batch_history/batch_history.dart';
-import 'package:vidyaveechi_website/model/batch_history_model/batch_history_model.dart';
-import 'package:vidyaveechi_website/view/colors/colors.dart';
 import 'package:vidyaveechi_website/view/fonts/text_widget.dart';
-import 'package:vidyaveechi_website/view/widgets/responsive.dart';
 import 'package:vidyaveechi_website/view/widgets/table/table_widgets.dart';
 
 class BatchHistroyListPage extends StatelessWidget {
@@ -12,92 +8,91 @@ class BatchHistroyListPage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      padding: EdgeInsets.all(8),
-      color: screenContainerbackgroundColor,
-      // height: ResponsiveWebSite.isMobile(context) ? 890 : 820,
-      width: 1200,
-      child: Column(
-        crossAxisAlignment: CrossAxisAlignment.start,
-        children: [
-          const Padding(
-            padding: const EdgeInsets.only(left: 30, top: 30, bottom: 30,),
-            child: const TextFontWidget(
-              text: 'List Of Batches',
-              fontsize: 18,
-              fontWeight: FontWeight.bold,
-            ),
-          ),
-          SingleChildScrollView(
-            scrollDirection: Axis.horizontal,
-            child: SizedBox(
-              width: 1200,
-              height: 800,
-              child: Padding(
-                padding: const EdgeInsets.only(left: 30),
-                child: Column(
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        const TextFontWidget(
+          text: 'List Of Batch',
+          fontsize: 18,
+          fontWeight: FontWeight.bold,
+        ),
+        SingleChildScrollView(
+          scrollDirection: Axis.horizontal,
+          child: SizedBox(
+            width: 870,
+            height: 800,
+            child: Column(
+              children: [
+                 const Row(
                   children: [
-                    const Row(
-                      children: [
-                        HeaderOfTable(text: "No.", flex: 2),
-                        HeaderOfTable(text: "Batch Name", flex: 5),
-                        HeaderOfTable(text: "Batch Created Date", flex: 5),
-                        HeaderOfTable(text: "Create By", flex: 4),
-                        HeaderOfTable(text: "Total Students", flex: 4),
-                        HeaderOfTable(text: "Total Staff", flex: 4),
-                      ],
-                    ),
-                    Expanded(
-                      child: StreamBuilder<List<BatchHistoryModel>>(
-                        stream: Get.put(BatchHistoryController()).batchHistoryStream(),
-                        builder: (context, snapshot) {
-                          List<BatchHistoryModel> batchHistory = snapshot.data ?? [];
-                          return ListView.separated(
-                              itemBuilder: (context, index) {
-                                return SizedBox(
-                                  width: 870,
-                                  height: 45,
-                                  child: Row(
-                                    children: [
-                                      TableListContainers(text: '${index + 1}', flex: 2),
-                                      const TableListContainers(
-                                        text: "THI51685326589",
-                                        flex: 5,
-                                      ),
-                                      const TableListContainers(
-                                        text: "23/02/2020",
-                                        flex: 5,
-                                      ),
-                                      const TableListContainers(
-                                        text: "Admin",
-                                        flex: 4,
-                                      ),
-                                      const TableListContainers(
-                                        text: "140",
-                                        flex: 4,
-                                      ),
-                                      const TableListContainers(
-                                        text: "15",
-                                        flex: 4,
-                                      ),
-                                    ],
-                                  ),
-                                );
-                              },
-                              separatorBuilder: (context, index) => const SizedBox(
-                                    height: 1,
-                                  ),
-                              itemCount: 1);
-                        },
-                      ),
-                    ),
+                    HeaderOfTable(text: "No.", flex: 1),
+                    HeaderOfTable(text: "Batch Name", flex: 3),
+                    HeaderOfTable(text: "Batch Created Date", flex: 2),
+                    HeaderOfTable(text: "Create By", flex: 2),
+                    // HeaderOfTable(text: "Total Students", flex: 2),
+                    // HeaderOfTable(text: "Total Staff", flex: 2),
                   ],
                 ),
-              ),
+                Expanded(
+              
+                  child: StreamBuilder(
+                    stream: FirebaseFirestore.instance.collection('SchoolListCollection').snapshots(),
+                    builder: (context,snapshot) {
+ final List<DocumentSnapshot> documents = snapshot.data!.docs;                    
+    if(snapshot.hasData){
+return  ListView.separated(
+                                  itemBuilder: (context, index) {
+final document = documents[index].data() as Map<String, dynamic>; 
+      final DateTime createDate = (document['createDate'] as Timestamp).toDate() ; 
+     //  final String formattedCreateDate = formatDate(createDate); 
+                                
+ return SizedBox(
+                                      width: 870,
+                                      height: 45,
+                                      child: Row(
+                                        children: [
+                                          TableListContainers(text: '${index + 1}', flex: 2),
+                                           TableListContainers(
+                                            text:document['batchYear'] ?? '',
+                                            flex: 5,
+                                          ),
+                                           TableListContainers(
+                                            text:createDate.toString(),
+                                      
+                                            flex: 5,
+                                          ),
+                                           TableListContainers(
+                                            text: document['adminUserName'] ?? '',
+                                            flex: 4,
+                                          ),
+                                          //  TableListContainers(
+                                          //   text: document['batchYear'] ?? '',
+                                          //   flex: 4,
+                                          // ),
+                                          //  TableListContainers(
+                                          //   text: document['batchYear'] ?? '',
+                                          //   flex: 4,
+                                          // ),
+                                        ],
+                                      ),
+                                    );
+                                  },
+                                  separatorBuilder: (context, index) => const SizedBox(
+                                        height: 1,
+                                      ),
+                                  itemCount: 1);
+    }else{
+return const Text('No data found');   
+    }
+
+                    }
+                  ),
+                ),
+              ],
             ),
           ),
-        ],
-      ),
+        ),
+      ],
     );
   }
 }
